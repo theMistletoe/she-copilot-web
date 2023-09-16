923a475f6bb7506ec35a00d168a7d7391f9a1548
Motoaki Tanaka <katachiwotodomete@gmail.com>
2023-09-16 14:12:17 +0000

    プロンプト修正

--- app/api/route.ts
+++ app/api/route.ts
@@ -13,10 +13,10 @@ export async function POST(request: NextRequest) {
     
     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
       SystemMessagePromptTemplate.fromTemplate(
-        "あなたは提示された文章の文脈や構造を理解し、人間が理解しやすい形に構造化・整理することが得意です。\n"
+        "あなたは提示された文章の文脈や構造を理解し、理解しやすい形に構造化・整理することが得意です。出力するのはMarkdownのプレーンテキストです。"
         ),
-        HumanMessagePromptTemplate.fromTemplate("以下の文章の文脈を汲み取り、誤字脱字と推測される部分を適切に修正した上で、人間が理解しやすい形で構造化したMarkdown形式で出力してください。返却するのはあなたが整理したMarkdownテキストのみにしてください。「{text}」\n"),
-        HumanMessagePromptTemplate.fromTemplate("以下が結果のMarkdownテキストです。\n"),
+        HumanMessagePromptTemplate.fromTemplate("以下の文章の文脈を汲み取り、誤字脱字と推測される部分を適切に修正した上で、人間が理解しやすい形で構造化したMarkdown形式で出力してください。出力するのはあなたが整理したMarkdownプレーンテキストのみにしてください。「{text}」"),
+        HumanMessagePromptTemplate.fromTemplate("===以下が出力結果==="),
       ]);
       
       const chat_prompt = (await chatPrompt.formatPromptValue({
@@ -29,6 +29,7 @@ export async function POST(request: NextRequest) {
       });
       
       const response = await chat.call(chat_prompt);
+      console.log(response);
       
       return NextResponse.json({reply: response.content});
     } catch (error) {
